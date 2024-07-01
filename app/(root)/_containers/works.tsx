bba64f4dae7c1a1d04dd885bc716463368e09617
DewaSRY <sdewa6645@gmail.com>
2024-07-01 20:41:40 +0800

    update:fill works data

--- app/(root)/_containers/works.tsx
+++ app/(root)/_containers/works.tsx
@@ -52,11 +52,11 @@ export default function Works({
       <Heading.H2 className="mx-6 underline">Work Experience</Heading.H2>
 
       <WorkCard.WorkCard className="work-card transition-transform scale-0 translate-x-[-100%]">
-        <Heading.H3>Freelance Frontend Developer</Heading.H3>
+        <Heading.H3>Full Stack Engineer</Heading.H3>
         <Paragraph.Description>
-          Angular-Base warehouse data management system
+          Develope Money Changer Application
         </Paragraph.Description>
-        <Paragraph.Date>January,2023-November, 2023</Paragraph.Date>
+        <Paragraph.Date> February, 2024 - April, 2024</Paragraph.Date>
         <WorkCard.WorkBody>
           <Image
             src="/works/money_exchanger_1.png"
@@ -65,20 +65,37 @@ export default function Works({
             alt="warehouse_dumy"
           />
           <WorkCard.WorkUl>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Created a user-friendly money changer dashboard. Structured
+              application UI components to make it easier to understand and
+              interact with the application.
+            </WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Secured business transactions by integrating with local banks.
+              Application collects and secure user data and checks user abnormal
+              transaction history.
+            </WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Improved application security transactions with opaque refresh
+              token system, proved to achieve a 200% improvement in security
+              compared to JWT refresh tokens.
+            </WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Compressed application to minimize size without affecting the
+              application functions. Containerized application small and secure
+              images, success increase security and reduce container size by 80%
+              compared to debian base image.
+            </WorkCard.WorkLi>
           </WorkCard.WorkUl>
         </WorkCard.WorkBody>
       </WorkCard.WorkCard>
 
       <WorkCard.WorkCard className="work-card transition-transform scale-0 translate-x-[-100%]">
-        <Heading.H3>Freelance Frontend Developer</Heading.H3>
+        <Heading.H3>Frontend Engineer</Heading.H3>
         <Paragraph.Description>
-          Angular-Base warehouse data management system
+          Warehouse Management System
         </Paragraph.Description>
-        <Paragraph.Date>January,2023-November, 2023</Paragraph.Date>
+        <Paragraph.Date> January,2023 - November, 2023</Paragraph.Date>
         <WorkCard.WorkBody>
           <Image
             src="/works/warehouse_dumy.png"
@@ -87,10 +104,25 @@ export default function Works({
             alt="warehouse_dumy"
           />
           <WorkCard.WorkUl>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
-            <WorkCard.WorkLi>i have do this one</WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Developed warehouse dashboard and monitor application,
+              accomplished efficient inventory tracking by developing a
+              warehouse management data system, resulting in improved monitoring
+              of storage items.
+            </WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Integrated the application with CCTV video and maintained
+              application performance by 80%, in high computing processes using
+              webassembly.
+            </WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Improved usability by developed user-friendly interface enabling
+              warehouse staff to easily operate the system.
+            </WorkCard.WorkLi>
+            <WorkCard.WorkLi>
+              Facilitated application with quick access to critical information,
+              ensuring warehouse operations were streamlined and effective.
+            </WorkCard.WorkLi>
           </WorkCard.WorkUl>
         </WorkCard.WorkBody>
       </WorkCard.WorkCard>
