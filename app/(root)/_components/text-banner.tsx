62487034377d169e04ebcaf1360366d61974eaa3
DewaSRY <sdewa6645@gmail.com>
2024-07-04 15:53:10 +0800

    fix:herro banner invalid character

--- app/(root)/_components/text-banner.tsx
+++ app/(root)/_components/text-banner.tsx
@@ -12,22 +12,16 @@ export default function TextBanner({
 }: TextBannerProps) {
   return (
     <div className={cn(className, "w-11/12 md:w-8/12 ")}>
-      <Paragraph.Description>
-        <span className="text-[18px] md:text-[22px] xl:text-[26px] mr-2 text-primary-one">
-          HALLO,
-        </span>
-        my name is
-      </Paragraph.Description>
+      <span className="text-[18px] md:text-[22px] xl:text-[26px] mr-2 text-primary-one">
+        HALLO my name is
+      </span>
       <Paragraph.Description className="text-[18px] md:text-[20px] xl:text-[24px]">
         Dewa Surya Ariesta
       </Paragraph.Description>
-      <hr className="w-8/12" />
-      <Paragraph.Description>
-        <span className="text-[18px] md:text-[20px] xl:text-[24px] mr-2 text-primary-one">
-          I am
-        </span>
-        a
-      </Paragraph.Description>
+      <hr className="w-8/12 h-2 p-[2px]" />
+      <span className="text-[18px] md:text-[20px] xl:text-[24px] mr-2 text-primary-one">
+        I am a
+      </span>
       <Heading.H1>Full Stack Web Developers</Heading.H1>
       <hr className="h-2 p-[2px]" />
       <Paragraph.Description>Let's make miracle happen</Paragraph.Description>
