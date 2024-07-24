3a65f0d386d3ef117288006ccf12af70396334d5
DewaSRY <sdewa6645@gmail.com>
2024-07-24 10:28:01 +0800

    add:finishes make navigation for small size

--- app/(root)/_components/hamburger-button.tsx
+++ app/(root)/_components/hamburger-button.tsx
@@ -17,26 +17,26 @@ export default function HamburgerButton({
     <button
       {...resProps}
       type="button"
-      className="text-white-two w-[50px] h-[50px] relative duration-150 ease-out transition-transform hover:scale-95"
+      className="text-white-two w-[40px] h-[40px] relative duration-150 ease-out transition-transform hover:scale-95"
     >
       <hr
         className={cn(
           "duration-300 ",
-          "border-white-two border-2 w-full absolute top-[20%]",
+          "border-white-two border w-full absolute top-[20%]",
           `${isActive ? "rotate-45 top-[47%]" : ""}`
         )}
       />
       <hr
         className={cn(
           "duration-300",
-          "border-white-two border-2  w-full absolute top-[47%]",
+          "border-white-two border  w-full absolute top-[47%]",
           `${isActive ? "opacity-0" : ""}`
         )}
       />
       <hr
         className={cn(
           "duration-300",
-          "border-white-two border-2  w-full absolute bottom-[20%]",
+          "border-white-two border  w-full absolute bottom-[20%]",
           `${isActive ? "rotate-[-45deg] bottom-[47%]" : ""}`
         )}
       />
--- app/(root)/_containers/navigation-small.tsx
+++ app/(root)/_containers/navigation-small.tsx
@@ -1,12 +1,18 @@
 "use client";
-import { ComponentProps, PropsWithChildren, useState } from "react";
+import {
+  ComponentProps,
+  ElementRef,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { usePathname } from "next/navigation";
 
 import HamburgerButton from "../_components/hamburger-button";
 
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 import SocialCTA from "../_content/social-cta";
 import RouteNavigation from "../_content/route-navigation";
@@ -21,17 +27,47 @@ export default function NavigationSmall({
   ...resProps
 }: NavigationSmallProps) {
   const [isActive, setIsActive] = useState(false);
+  const overLayElement = useRef<ElementRef<"div">>(null);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsActive(false); // every element get render or re-render the overlay get in-active
+
+    function resizeHandler() {
+      setIsActive(false);
+    }
+    window.addEventListener("resize", resizeHandler);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, [pathname]); // ever phat name of rout change the element get re-render
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+      tl.to(overLayElement.current, {
+        scaleY: isActive ? 1 : 0,
+        ease: "power4.inOut",
+      });
+    },
+    {
+      dependencies: [isActive],
+    }
+  );
   return (
     <>
-      <div className="xl:hidden fixed top-4 right-2 z-10">
+      <div className="xl:hidden fixed top-4 right-2 z-10 ">
         <HamburgerButton
           isActive={isActive}
           onClick={setIsActive.bind(null, !isActive)}
         />
       </div>
-      <div className="fixed inset-0 z-[9] xl:hidden bg-white-alfa flex text-center items-center">
+      <div
+        ref={overLayElement}
+        className="origin-top fixed inset-0 z-[9]  xl:hidden bg-white-alfa flex text-center items-center"
+      >
         <SocialCTA className="absolute  grid grid-rows-3 gap-8  " />
-        <RouteNavigation className="w-[200px] translate-x-[calc(50vw-50%)] gap-6" />
+        <RouteNavigation className="w-[200px] translate-x-[calc(50vw-50%)] flex flex-col gap-8" />
       </div>
     </>
   );
